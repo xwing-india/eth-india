d240b00c29349be3a72f04af27a667f90aee256a
taxio <taxio.2029@gmail.com>
2022-12-04 05:11:43 +0530

    fix backend

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -2,11 +2,10 @@ import { Request, Response } from "firebase-functions";
 import * as functions from "firebase-functions";
 
 import { initializeApp } from "firebase-admin/app";
-import { UserOperationStruct } from "@account-abstraction/contracts";
+// eslint-disable-next-line camelcase
+import {EntryPoint__factory, UserOperationStruct} from "@account-abstraction/contracts";
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const Web3 = require("web3");
 import { ethers } from "ethers";
-import { EntryPointABI } from "./consts";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cors = require("cors")({ origin: true });
@@ -34,8 +33,7 @@ export const runOp = functions.https.onRequest(
         `https://${req.body.network}.infura.io/v3/${infuraApiKey}`
       );
 
-      // const op: UserOperationStruct = req.body.op;
-      // console.log(op);
+      console.log(req.body.op);
 
       // EntryPoint コントラクトの `handleOps` を叩く
       const txHash = await callHandleOps(provider, req.body.op);
@@ -49,15 +47,6 @@ export const runOp = functions.https.onRequest(
   }
 );
 
-const newWeb3 = (network: string) => {
-  const infuraApiKey = process.env.INFURA_API_KEY;
-  return new Web3(
-    new Web3.providers.HttpProvider(
-      `https://${network}.infura.io/v3/${infuraApiKey}`
-    )
-  );
-};
-
 const callHandleOps = async (
   provider: ethers.providers.JsonRpcProvider,
   op: UserOperationStruct
@@ -70,21 +59,11 @@ const callHandleOps = async (
   if (entryPointContractAddress === undefined) {
     throw new Error("ENTRY_POINT_ADDRESS is not set");
   }
-  const bundlerAddress = process.env.BUNDLER_ADDRESS;
-  if (bundlerAddress === undefined) {
-    throw new Error("BUNDLER_ADDRESS is not set");
-  }
 
   const wallet = new ethers.Wallet(bundlerPrivateKey, provider);
 
-  const contract = new ethers.Contract(
-    entryPointContractAddress,
-    JSON.parse(EntryPointABI),
-    wallet
-  );
-  console.log(contract);
-  const tx = contract.handleOps([op], bundlerAddress);
-  console.log(`tx: `, tx);
-
-  return "0x123";
+  // eslint-disable-next-line camelcase
+  const contract = EntryPoint__factory.connect(entryPointContractAddress, wallet);
+  const tx = await contract.handleOps([op], wallet.address);
+  return tx.hash;
 };
