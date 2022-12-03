11252a0092b6648d667ec6e939c5cf8c95bc4bb1
taxio <taxio.2029@gmail.com>
2022-12-03 23:21:35 +0530

    tweak

--- backend/functions/src/consts.ts
+++ backend/functions/src/consts.ts
@@ -1,7 +1,3 @@
-export const AccountFactoryContractAddress = "0x71dB99931ECd9c41A0A3eb004100F157697443Ee";
-export const BundlerWalletAddress = "0x5Ab1eA0a966aA678845Fa70a7BC890127B7Dc55A";
-export const EntryPointContractAddress = "0xb59fe658170fA89736cC8414D1a354ca21054A4A";
-
 export const EntryPointABI = `[{
 "inputs": [
   {
