6ea5ce7f6021a4bc9dfa2c4f51a5dc2f865bdab5
taxio <taxio.2029@gmail.com>
2022-12-03 13:54:51 +0530

    impl `send` function

--- frontend/util/DemoAccountAPI.ts
+++ frontend/util/DemoAccountAPI.ts
@@ -1,7 +1,8 @@
-import {BigNumber, BigNumberish, Signer, Wallet} from "ethers";
+import {BigNumber, BigNumberish, ethers, Signer, Wallet} from "ethers";
 import {BaseApiParams, BaseAccountAPI} from "@account-abstraction/sdk/dist/src/BaseAccountAPI";
 import {SimpleAccount} from "@account-abstraction/contracts";
 import {arrayify} from "ethers/lib/utils";
+import Web3 from "web3";
 
 export interface DemoAccountApiParams extends BaseApiParams {
   personaSigner: Signer;
@@ -45,22 +46,39 @@ export class DemoAccountAPI extends BaseAccountAPI {
   }
 }
 
-const sampleRun = (password: string) => {
-  // TODO: password から秘密鍵を作る
-  const personaSigner = new Wallet("");
+const send = async (network: string, personaPassword: string, from: string, to: string, value: number) => {
+  // password から秘密鍵を作る
+  const infuraApiKey = process.env.INFURA_API_KEY;
+  const providerHost = `https://${network}.infura.io/v3/${infuraApiKey}`
+  const web3 = new Web3(new Web3.providers.HttpProvider(providerHost));
+  const personaAccount = web3.eth.accounts.create(personaPassword);
+
+  // UserOperation の組み立て
   const api = new DemoAccountAPI({
     accountContract: undefined, // TODO: AA ウォレットへのアクセス用インスタンスを作る
-    personaSigner: personaSigner,
+    personaSigner: new Wallet(personaAccount.privateKey),
     entryPointAddress: "",
-    provider: undefined, // TODO: infura とか
-  });
-
-  // Note: target と data は `call data` に埋め込まれる
-  const op = api.createSignedUserOp({
-    target: "", // TODO: 送り先のアドレス
-    data: "", // TODO: 具体的な処理のデータ
+    provider: ethers.getDefaultProvider(providerHost),
   })
 
-  // TODO: Bundler API に投げる
-  // {"op": op} みたいな Request Body を Cloud Function の runOp に投げる
+  const op = api.createSignedUserOp({
+    target: to, // 送り先のアドレス
+    data: "", // TODO: 具体的な処理のデータ
+  });
+
+  // Bundler API を叩く
+  const bundlerEndpoint = process.env.BUNDLER_ENDPOINT;
+  if (bundlerEndpoint === undefined) {
+    throw new Error("BUNDLER_ENDPOINT is not set");
+  }
+  await fetch(bundlerEndpoint, {
+    method: "POST",
+    headers: {
+      'content-type': 'application/json',
+    },
+    body: JSON.stringify({
+      "network": network,
+      "op": op,
+    }),
+  })
 };
