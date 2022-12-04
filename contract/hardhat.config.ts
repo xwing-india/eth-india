52c45ad2cd7c5d7af2a1d6118386387cdd161761
inaridiy <inainainariinainari@gmail.com>
2022-12-04 07:17:03 +0530

    add chain data

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const config: HardhatUserConfig = {
@@ -11,6 +10,18 @@ const config: HardhatUserConfig = {
       url: "https://goerli.infura.io/v3/ddac2247725f422196229bfba8ac3877",
       accounts: [process.env.PRIVATE_KEY || ""],
     },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    cronos:{
+      url: "https://evm-t3.cronos.org/",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+    chiado:{
+      url:"https://rpc.eu-central-2.gateway.fm/v3/gnosis/archival/chiado",
+      accounts: [process.env.PRIVATE_KEY || ""],
+    }
   },
   etherscan: {
     apiKey: process.env.ETH_SCAN,
